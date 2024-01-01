02eb32b7fb702eaa73d00e6607051e791b999289
Jonathan DeBarro <jdebarro@calpoly.edu>
2023-12-31 21:52:42 -0800

    Added initial files

---
+++ content.js
@@ -0,0 +1,4 @@
+// Get HTML and send it to extension script
+// Read the HTML content of the current page
+var page_html = document.documentElement.outerHTML;
+chrome.runtime.sendMessage({type: html, content: page_html});
